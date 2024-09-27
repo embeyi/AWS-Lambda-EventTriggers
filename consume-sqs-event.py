8a7ed779b65051235d7b3177ae819c359df5c1ea
Ikechi Mbeyi <133457406+embeyi@users.noreply.github.com>
2024-09-27 10:02:24 +0500

    Create consume-sqs-event.py

---
+++ consume-sqs-event.py
@@ -0,0 +1,23 @@
+import json
+
+def lambda_handler(event, context):
+    print(' Hi SQS Triggerred me')
+    # Loop through each record in the event
+    for record in event['Records']:
+        # Extract the body of the SQS message
+        message_body = record['body']
+        
+        # Print the entire message body
+        print("Message Body:", message_body)
+        
+        # If the message body is JSON formatted, you can parse it
+        try:
+            message = json.loads(message_body)
+            print("Parsed Message:", json.dumps(message, indent=2))
+        except json.JSONDecodeError:
+            print("Message is not in JSON format.")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Processed SQS messages')
+    }
